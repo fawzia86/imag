7cd8010e18a2f3882a198c0e45f1c3ed07986106
fawzia86 <99467457+fawzia86@users.noreply.github.com>
2024-06-23 19:26:35 +0700

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <a href="/">Home</a> |
+                <a href="/">About</a> |
+                <a href="https://wa.me/6285328007533">Contact</a>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
